291777c8a7a31b695fb58ee943928def70c9f70d
LennyMX <patron.luis@uabc.edu.mx>
2026-08-17 22:47:06 -0700

    Add Persona class with attributes and methods

---
+++ Persona.java
@@ -0,0 +1,51 @@
+package com.example.crudd.modelo;
+
+public class Persona {
+
+    private int id;
+    private String nombre;
+    private String direccion;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public Persona(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
